52c830dd707f6c6ce71d226c7ab6d5bf6d6eba3b
Mehdi Kaouache <kaouache.mehdi@gmail.com>
2026-09-12 18:35:08 -0400

    configurer le prefixe api et le versionnement URI v1

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule, ObserveInstrument } from './app.module';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     instrument: ObserveInstrument,
+  });
+  app.setGlobalPrefix('api');
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1'
   });
   await app.listen(process.env.PORT ?? 3000);
 }
