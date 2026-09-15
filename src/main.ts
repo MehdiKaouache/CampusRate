bd36f9d65698798ebdf3d9145c1fec699cfab6ce
Mehdi Kaouache <kaouache.mehdi@gmail.com>
2026-09-15 15:47:36 -0400

    retirer les composants et le module

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,16 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule, ObserveInstrument } from './app.module';
 import { VersioningType } from '@nestjs/common';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    instrument: ObserveInstrument,
-  });
+  const app = await NestFactory.create(AppModule);
+
   app.setGlobalPrefix('api');
   app.enableVersioning({
     type: VersioningType.URI,
-    defaultVersion: '1'
+    defaultVersion: '1',
   });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
